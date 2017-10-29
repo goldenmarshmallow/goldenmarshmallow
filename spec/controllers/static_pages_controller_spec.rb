3e9587e7da913f6f15b31002077c3a3bb05232e3
Gamal Al-Hetari <gn62192@gmail.com>
2017-10-29 22:50:33 +0000

    fixed errors on rspec

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-    expect(true).to eq true
+    describe 'static_pages#index' do
+        it "will successfully show the index page" do
+           get :index
+           expect(response).to have_http_status(:success)
+        end
+    end
 end
