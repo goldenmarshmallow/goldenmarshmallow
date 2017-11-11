084db839d60da750c2dad97283f4292ecc406a9f
Christian Van <khanhvan121992@gmail.com>
2017-11-11 15:30:07 +0000

    Fixed the tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  describe "game#new action" do
-    it 'should check the original position of some pieces' do
+  context "When a new game is created" do
+    it 'will generate all the pieces in their correct positions' do
       game = FactoryBot.create(:game)
-      #get :show, params: { id: game.id }
 
-      expect(game.pieces.at(3, 1).to_s).to eq 'Pawn'
-      expect(game.pieces.at(1, 0).to_s).to eq 'Knight'
-      expect(game.pieces.at(7, 0).to_s).to eq 'Rook'
-      expect(game.pieces.at(6, 7).to_s).to eq 'Knight'
-      expect(game.pieces.at(6, 6).to_s).to eq 'Pawn'
-      expect(game.pieces.at(0, 4).to_s).to eq 'King'
-      expect(game.pieces.at(3, 7).to_s).to eq 'Queen'
-      expect(game.pieces.at(5, 7).to_s).to eq 'Bishop'
-      expect(game.pieces.at(2, 0).to_s).to eq 'Bishop'
+      expect(game.pieces.at(3, 1).type).to eq 'Pawn'
+      expect(game.pieces.at(1, 0).type).to eq 'Knight'
+      expect(game.pieces.at(7, 0).type).to eq 'Rook'
+      expect(game.pieces.at(6, 7).type).to eq 'Knight'
+      expect(game.pieces.at(6, 6).type).to eq 'Pawn'
+      expect(game.pieces.at(4, 0).type).to eq 'King'
+      expect(game.pieces.at(3, 7).type).to eq 'Queen'
+      expect(game.pieces.at(5, 7).type).to eq 'Bishop'
+      expect(game.pieces.at(2, 0).type).to eq 'Bishop'
     end
   end
 end
