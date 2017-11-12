3384be17e54e9ee37a434984673729d5de1cac5c
Evan <evan.pineo@gmail.com>
2017-11-12 14:01:02 +0000

    Add an available scope to games so that we can call Game.available

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,8 @@ class Game < ApplicationRecord
   validates :name, presence: true
   belongs_to :user
   has_many :pieces
+
+  def self.available
+    where('game.user.count < 2') if game.user.count > 0
+  end
 end
