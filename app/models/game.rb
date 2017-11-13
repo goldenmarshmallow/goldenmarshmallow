b664c9b13cd9a9a9e190c16515ae733cdcf60be2
Gamal Al-Hetari <gn62192@gmail.com>
2017-11-13 17:49:35 +0000

    new branch with 3 files being changed

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,41 @@
 class Game < ApplicationRecord
   validates :name, presence: true
+  after_create :populate_board!
   belongs_to :user
   has_many :pieces
+  def populate_board!
+    (0..7).each do |i|
+      Pawn.create(
+        game_id: id,
+        x_position: i,
+        y_position: 1,
+        color: :white
+      )
+    end
+    Rook.create(game_id: id, x_position: 0, y_position: 0, color: :white)
+    Rook.create(game_id: id, x_position: 7, y_position: 0, color: :white)
+    Knight.create(game_id: id, x_position: 1, y_position: 0, color: :white)
+    Knight.create(game_id: id, x_position: 6, y_position: 0, color: :white)
+    Bishop.create(game_id: id, x_position: 2, y_position: 0, color: :white)
+    Bishop.create(game_id: id, x_position: 5, y_position: 0, color: :white)
+    Queen.create(game_id: id, x_position: 3, y_position: 0, color: :white)
+    King.create(game_id: id, x_position: 4, y_position: 0, color: :white)
+    # black pieces
+    (0..7).each do |i|
+      Pawn.create(
+        game_id: id,
+        x_position: i,
+        y_position: 6,
+        color: :black
+      )
+    end
+    Rook.create(game_id: id, x_position: 0, y_position: 7, color: :black)
+    Rook.create(game_id: id, x_position: 7, y_position: 7, color: :black)
+    Knight.create(game_id: id, x_position: 1, y_position: 7, color: :black)
+    Knight.create(game_id: id, x_position: 6, y_position: 7, color: :black)
+    Bishop.create(game_id: id, x_position: 2, y_position: 7, color: :black)
+    Bishop.create(game_id: id, x_position: 5, y_position: 7, color: :black)
+    Queen.create(game_id: id, x_position: 3, y_position: 7, color: :black)
+    King.create(game_id: id, x_position: 4, y_position: 7, color: :black)
+  end
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
-
 RSpec.describe Piece, type: :model do
+  it 'will check if a piece is obstructed' do
+    game = FactoryBot.create(:game)
+    expect(game.pieces.at(3, 1).obstructed?(3, 3)).to eq false
+    expect(game.pieces.at(0, 0).obstructed?(0, 4)).to eq true
+    expect(game.pieces.at(0, 7).obstructed?(2, 7)).to eq true
+    expect(game.pieces.at(1, 0).obstructed?(2, 2)).to eq 'Invalid Input'
+  end
 end
