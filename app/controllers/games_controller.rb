8048d7a7dba5e6d79eccf503f14a766ca2bb4e82
Christian Van <khanhvan121992@gmail.com>
2017-11-15 09:11:03 +0000

    Correct Rubocop violation

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,7 +16,7 @@ class GamesController < ApplicationController
   end
 
   def show
-    @game = Game.find_by_id(params[:id])
+    @game = Game.find_by(id: params[:id])
     return render_not_found if @game.blank?
   end
 
