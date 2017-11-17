a03f63b092477991ce35ec6584817edb40e55148
rodpat <rodpat@gmail.com>
2017-11-17 02:27:30 +0000

    Adding valid_move in King model

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -15,4 +15,16 @@ RSpec.describe Piece, type: :model do
     queen = FactoryBot.create(:piece, x_position: 3, y_position: 0, color: :white, type: :queen, game: game)
     expect(queen.obstructed?(3, 3)).to eq false
   end
+
+  it 'will check if a king\'s move is valid' do
+    game = FactoryBot.create(:game)
+    king = FactoryBot.create(:king, x_position: 4, y_position: 0, color: :white, type: :king, game: game)
+    expect(king.valid_move?(4, 1)).to eq true
+    king = FactoryBot.create(:king, x_position: 4, y_position: 0, color: :white, type: :king, game: game)
+    expect(king.valid_move?(4, 3)).to eq false
+    king = FactoryBot.create(:king, x_position: 4, y_position: 0, color: :white, type: :king, game: game)
+    expect(king.valid_move?(4, 0)).to eq true
+    king = FactoryBot.create(:king, x_position: 4, y_position: 3, color: :white, type: :king, game: game)
+    expect(king.valid_move?(5, 0)).to eq false
+  end
 end
