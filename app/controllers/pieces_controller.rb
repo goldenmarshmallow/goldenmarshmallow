63a36b9a43fa5abb827e687efd7ba4da689e5376
Christian Van <khanhvan121992@gmail.com>
2017-11-22 11:18:08 +0000

    Add first two steps for moving pieces

---
+++ app/controllers/pieces_controller.rb
@@ -0,0 +1,23 @@
+class PiecesController < ApplicationController
+  def create
+    @piece = current_game.pieces.create(piece_params)
+  end
+
+  def show
+    @piece = Piece.find(params[:id])
+  end
+
+  def update
+  end
+
+  private
+
+  def piece_params
+    params.require(:piece).permit(:type)
+  end
+
+  helper_method :current_game
+  def current_game
+    @current_game ||= Game.find(params[:id])
+  end
+end
