e299af762e9b446930485f057da1b7dd610e3ecd
Gamal Al-Hetari <gn62192@gmail.com>
2017-11-24 23:19:57 +0000

    queen's valid move method was created as well as few tests that passes

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -2,4 +2,11 @@ class Queen < Piece
   def to_unicode
     black? ? "\u265B" : "\u2655"
   end
+  
+  def valid_move?(destination_x, destination_y)
+    distance_x = (destination_x - x_position).abs
+    distance_y = (destination_y - y_position).abs
+
+    distance_x == distance_y || distance_x == 0 || distance_y == 0
+  end
 end
---
+++ spec/models/queen_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+RSpec.describe Queen, type: :model do
+    it 'will check if a queen\'s move is valid' do
+        game = FactoryBot.create(:game)
+        queen = FactoryBot.create(:queen, x_position: 3, y_position: 0, color: :white, type: :queen, game: game)
+        expect(queen.valid_move?(3,0)).to eq true
+        expect(queen.valid_move?(4,2)).to eq false
+        expect(queen.valid_move?(7,4)).to eq true
+        
+        queen = FactoryBot.create(:queen, x_position: 5, y_position: 4, color: :white, type: :queen, game: game)
+        expect(queen.valid_move?(7,5)).to eq false
+        expect(queen.valid_move?(1,3)).to eq false
+        expect(queen.valid_move?(7,6)).to eq true
+    end
+end
