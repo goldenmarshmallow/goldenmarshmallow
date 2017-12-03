0ec30f92a7034f5b967130643de763ac7f5b9d63
Evan <evan.pineo@gmail.com>
2017-12-03 22:45:08 +0000

    Add valid_move method and tests for Rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -2,4 +2,11 @@ class Rook < Piece
   def to_unicode
     black? ? "\u265C" : "\u2656"
   end
+
+  def valid_move?(destination_x, destination_y)
+    distance_x = (destination_x - x_position).abs
+    distance_y = (destination_y - y_position).abs
+
+    distance_x == distance_y || distance_x.zero? || distance_y.zero?
+  end
 end
