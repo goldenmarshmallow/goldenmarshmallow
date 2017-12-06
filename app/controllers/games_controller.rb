b824962911d60ed5ee4bd70752bddbb408d4bc82
Gamal Al-Hetari <gn62192@gmail.com>
2017-12-06 17:59:46 +0000

    joining game is supported

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,6 +13,7 @@ class GamesController < ApplicationController
     @game = current_user.games.create(game_params)
     if @game.valid?
       redirect_to game_path(@game)
+      @game.update_attributes(white_player_id: @game.user_id)
       @game.populate_board
     else
       render :new, status: :unprocessable_entity
@@ -23,11 +24,22 @@ class GamesController < ApplicationController
     @game = Game.find_by(id: params[:id])
     return render_not_found if @game.blank?
   end
+  
+  def update
+    @game = Game.find_by(id: params[:id])
+    return render_not_found if @game.blank?
+    if @game.valid?
+      redirect_to game_path(@game)
+      @game.update_attributes(black_player_id: @game.user_id)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
 
   private
 
   def game_params
-    params.require(:game).permit(:name)
+    params.require(:game).permit(:name, :white_player_id, :black_player_id)
   end
 
   helper_method :current_game
